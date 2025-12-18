902e54e32dc59692a44603c99eaf59b5b6251713
bala-amypo <bala@amypo.in>
2025-12-18 06:04:03 +0000

    User commit

---
+++ src/main/java/com/example/demo/security/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.example.demo.security;
+
+import com.example.demo.model.AppUser;
+import com.example.demo.repository.AppUserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final AppUserRepository appUserRepository;
+
+    public CustomUserDetailsService(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser user = appUserRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + user.getRole());
+        return new User(user.getEmail(), user.getPassword(), Collections.singleton(authority));
+    }
+}
