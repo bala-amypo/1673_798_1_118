a53d368ffc1522323c2e21ae28a5bdd5daab7900
bala-amypo <bala@amypo.in>
2025-12-26 09:45:09 +0000

    User commit

--- src/main/java/com/example/demo/repository/AppUserRepository.java
+++ src/main/java/com/example/demo/repository/AppUserRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 @Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
     Optional<AppUser> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
--- src/main/java/com/example/demo/repository/DelayScoreRecordRepository.java
+++ src/main/java/com/example/demo/repository/DelayScoreRecordRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface DelayScoreRecordRepository extends JpaRepository<DelayScoreRecord, Long> {
     List<DelayScoreRecord> findBySupplierId(Long supplierId);
+    List<DelayScoreRecord> findByPoId(Long poId);
 }
